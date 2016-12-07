213952267152386c7ecf55cdf3fd4f79efe05f70
高橋真未 <takahashimami@takahashimami-no-macbook-air.local>
2016-12-07 17:31:27 +0900

    Add a

--- Gittest/ViewController.swift
+++ Gittest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     //コメント
     
-    aaaaaaaaaaaaaaaaaa
+    //aaaaaaaaaaaaaaaaaa
     
     override func viewDidLoad() {
         super.viewDidLoad()
