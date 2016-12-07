d09d4042b9323f380a6e2ee27a05971c9d470970
高橋真未 <takahashimami@takahashimami-no-macbook-air.local>
2016-12-07 17:33:08 +0900

    Add 変更

--- Gittest/ViewController.swift
+++ Gittest/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     //aaaaaaaaaaaaaaaaaa
     
+    //変更
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
